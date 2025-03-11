3dbf7b050d9f07431ba9ea0acf3059bea0ec0d58
Anner <anner.wang@icloud.com>
2025-03-11 09:24:09 +0800

    Add basic LLM interaction with OpenAI model using environment variables

--- src/main/java/com/anner/llm/LLM.java
+++ src/main/java/com/anner/llm/LLM.java
@@ -1,10 +1,16 @@
 package com.anner.llm;
 
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
 
-/**
- * @author Anner
- * @since 12.0
- * Created on 2025/3/7
- */
 public class LLM {
+    public static void main(String[] args) {
+        ChatLanguageModel model = OpenAiChatModel.builder()
+                .modelName(System.getenv("DOUBAO_MODEL_NAME"))
+                .baseUrl(System.getenv("DOUBAO_BASE_URL"))
+                .apiKey(System.getenv("DOUBAO_API_KEY"))
+                .build();
+
+        System.out.println(model.chat("你好，介绍一下自己的模型"));
+    }
 }
